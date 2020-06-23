6f9571c5610225a222b50082aac959313d594d52
Chris Yoon <chy.yoon@gmail.com>
2020-06-23 10:39:24 -0700

    updated details from template to match pig dice
    Co-authored-by: Mariel Hamson <mariel.hamson@gmail.com>

--- webpack.config.js
+++ webpack.config.js
@@ -19,7 +19,7 @@ plugins: [
   new CleanWebpackPlugin(),
   new Dotenv(),
   new HtmlWebpackPlugin({
-    title: 'My Project',
+    title: 'Die Piggy Die',
     template: './src/index.html',
     inject: 'body'
   })
